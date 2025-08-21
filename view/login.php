dbc72f0a305599c0befdf6f5865ffa2a1f7a07ae
nafim <nafim@gmail.com>
2025-08-21 22:04:43 +0600

    intial login page created

---
+++ view/login.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../model/login.css">
+    </head>
+    <body>
+        <div id="loginDiv">
+            <form action="" id="loginForm">
+            <h1>Login with your organization ID</h1>
+            <input type="text" id="orgId" placeholder="Enter your ID"><br>
+            <input type="password" id="password" placeholder="Password"><br>
+            <input type="submit" value="submit">
+            </form>
+        </div>
+
+    </body>
+</html>
