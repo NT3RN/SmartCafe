68857233007f96e5f761a56301846e495cdceaec
sadman <sadmanniloy5@gmail.com>
2025-09-24 15:42:15 +0600

    registration part update

--- view/login.php
+++ view/login.php
@@ -45,7 +45,7 @@
             </div>
             <input type="submit" name="submit" value="Login">
             <div class="forgetpass">
-                <p><a href="#">Can't access your account? </a></p>
+                <p><a href="register.php">Can't access your account? </a></p>
             </div>
         </form>
         <script src="js/login.js"></script>
